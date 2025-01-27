b85241678e45dcd91552f19b5ee3c0b0bd5bdbd2
MedSveta <sveta1978medved@gmail.com>
2025-01-27 21:07:44 +0200

    Added CssSelectors

---
+++ src/main/java/experiments/CssSelectors.java
@@ -0,0 +1,41 @@
+package experiments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CssSelectors {
+    WebDriver driver = new ChromeDriver();
+
+
+    @Test
+    public void selectorsHomePage(){
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        hideBanner();
+        hideFooter();
+        WebElement imgTools = driver.findElement(
+                By.cssSelector("img[src='/images/Toolsqa.jpg']"));
+        System.out.println(imgTools.getTagName());
+
+        WebElement divElements = driver.findElement(
+                By.cssSelector("div[class='avatar mx-auto white']"));
+        divElements.click();
+
+
+        //driver.quit();
+    }
+
+
+    private void hideBanner(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+    private void hideFooter(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
+    }
+}
