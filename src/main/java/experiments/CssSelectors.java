5cb83ce47aa68cabf3902b06f7a9fe180979f168
MedSveta <sveta1978medved@gmail.com>
2025-01-30 20:50:49 +0200

    Added CssSelectors+

--- src/main/java/experiments/CssSelectors.java
+++ src/main/java/experiments/CssSelectors.java
@@ -25,9 +25,57 @@ public class CssSelectors {
                 By.cssSelector("div[class='avatar mx-auto white']"));
         divElements.click();
 
-
-        //driver.quit();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
     }
+
+    @Test
+    public void selectorsElementsPage(){
+        driver.get("https://demoqa.com/elements");
+        driver.manage().window().maximize();
+        hideBanner();
+        WebElement texBox = driver.findElement(By.id("item-0"));
+        System.out.println(texBox.getAttribute("class"));
+        WebElement checkBox = driver.findElement(By.cssSelector("li[id='item-1']"));
+        checkBox.click();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+    @Test
+    public void selectorsRadioButtonPage(){
+        driver.get("https://demoqa.com/radio-button");
+        driver.manage().window().maximize();
+        hideBanner();
+        hideFooter();
+        //WebElement radioButtonYes = driver.findElement(By.cssSelector("label[for='yesRadio']"));
+        WebElement radioButtonYes = driver
+                .findElement(By.xpath("//label[@for='yesRadio']"));
+        radioButtonYes.click();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
+    }
+
+
+
+
+
+
 
 
     private void hideBanner(){
