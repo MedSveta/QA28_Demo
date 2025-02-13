1c5a3c1982a7a83b36b733c3808dca76e1813ce6
MedSveta <sveta1978medved@gmail.com>
2025-02-13 20:59:55 +0200

    Added Type Practice Form Enums

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -1,12 +1,17 @@
 package pages;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.util.List;
 
 public class PracticeFormPage extends BasePage {
 
@@ -29,6 +34,8 @@ public class PracticeFormPage extends BasePage {
     WebElement fieldDateOfBirth;
     @FindBy(id = "currentAddress")
     WebElement fieldAddress;
+    @FindBy(xpath = "//input[@id='subjectsInput']")
+    WebElement fieldSubject;
 
     public void typePracticeForm(Student student) {
         hideBanner();
@@ -36,22 +43,55 @@ public class PracticeFormPage extends BasePage {
         fieldName.sendKeys(student.getName());
         fieldLastName.sendKeys(student.getLastName());
         fieldEmail.sendKeys(student.getEmail());
+        typeGender(student.getGender());
         fieldMobile.sendKeys(student.getMobile());
         //fieldDateOfBirth.sendKeys(student.getDateOfBirth());
         typeDateOfBirth(student.getDateOfBirth());
+        typeSubjects(student.getSubject());
+        typeHobbies(student.getHobbies());
         fieldAddress.sendKeys(student.getAddress());
     }
 
-    private void typeDateOfBirth(String dateOfBirth){
-       fieldDateOfBirth.click();
-       String operationSystem = System.getProperty("os.name");
+    private void typeDateOfBirth(String dateOfBirth) {
+        fieldDateOfBirth.click();
+        String operationSystem = System.getProperty("os.name");
         System.out.println(operationSystem);
-        if(operationSystem.startsWith("Win"))
+        if (operationSystem.startsWith("Win"))
             fieldDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"));
         else if (operationSystem.startsWith("Mac"))
             fieldDateOfBirth.sendKeys(Keys.chord(Keys.COMMAND, "a"));
         fieldDateOfBirth.sendKeys(dateOfBirth);
         fieldDateOfBirth.sendKeys(Keys.ENTER);
+    }
+
+    private void typeGender(Gender gender) {
+        WebElement btnGender = driver.findElement(By.xpath(gender.getLocator()));
+        btnGender.click();
+    }
+
+    private void typeSubjects(String subjects) {
+        fieldSubject.click();
+        String[] arr = subjects.split(",");
+        for (String s : arr) {
+            fieldSubject.sendKeys(s);
+            fieldSubject.sendKeys(Keys.ENTER);
         }
     }
+
+    private void typeHobbies(List<Hobbies> hobbies) {
+        for (Hobbies h : hobbies) {
+            switch (h) {
+                case MUSIC:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case SPORTS:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+                case READING:
+                    driver.findElement(By.xpath(h.getLocator())).click();
+                    break;
+            }
+        }
+    }
+}
 
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,24 +1,35 @@
 package tests;
 
 import dto.Student;
+import enums.Gender;
+import enums.Hobbies;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticeFormTests extends AppManager {
 
-
+    //Maths,Physics,Chemistry
     @Test
-    public void studentRegFormPositiveTest(){
+    public void studentRegFormPositiveTest() {
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.MUSIC);
+        hobbies.add(Hobbies.READING);
+        hobbies.add(Hobbies.SPORTS);
+
         Student student = new Student("Pablo", "Poblito",
-                "pablo@meas.com", "male", "0123456789",
-                "12 Nov 2001", "Maths,Physiccs,Chemistry",
-                "sport", "", "street 1",
+                "pablo@meas.com", Gender.MALE, "0123456789",
+                "12 Nov 2001", "Maths,Physics,Chemistry",
+                hobbies, "", "street 1",
                 "NCR", "Delhi");
-    new HomePage(getDriver()).clickBtnForms();
-    new FormsPage(getDriver()).clickBtnPracticeForm();
-    new PracticeFormPage(getDriver()).typePracticeForm(student);
+
+        new HomePage(getDriver()).clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPracticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
     }
 }
